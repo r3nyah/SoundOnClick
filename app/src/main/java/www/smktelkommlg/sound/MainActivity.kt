83a04032004beec62123739e6f86531e594a90f6
Arian <arian_febrian_29rpl@student.smktelkom-mlg.sch.id>
2022-04-25 14:40:07 +0700

    added kobo

--- app/src/main/java/www/smktelkommlg/sound/MainActivity.kt
+++ app/src/main/java/www/smktelkommlg/sound/MainActivity.kt
@@ -1,31 +1,28 @@
 package www.smktelkommlg.sound
 
+import android.content.Intent
 import android.media.MediaPlayer
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import android.widget.ImageButton
-import android.widget.ImageView
-import android.widget.Toast
+
 
 class MainActivity : AppCompatActivity() {
-    //private lateinit var kobokanaer : ImageButton
-    private lateinit var mediaPlayer : MediaPlayer
+    private lateinit var mediaPlayer: MediaPlayer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val kobokanaer = findViewById<ImageButton>(R.id.kobokanaer)
-        mediaPlayer = MediaPlayer.create(this,R.raw.kobokanaer)
+        mediaPlayer = MediaPlayer.create(this, R.raw.kobokanaer)
         //Toast.makeText(this,"media playing", Toast.LENGTH_SHORT).show()
 
         //kobokanaer.setOnClickListener{
-            //mediaPlayer.start()
+        //mediaPlayer.start()
         //}
         kobokanaer.setOnClickListener{
-            mediaPlayer.start()
-
+        mediaPlayer.start()
+        startActivity(Intent(this@MainActivity, kobokanaer::class.java))
         }
     }
 
