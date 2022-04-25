101a2f8b7d3288545d3d30beb36cb2300581793e
Arian <arian_febrian_29rpl@student.smktelkom-mlg.sch.id>
2022-04-25 19:56:56 +0700

    solve sum bug

--- app/src/main/java/www/smktelkommlg/sound/MainActivity.kt
+++ app/src/main/java/www/smktelkommlg/sound/MainActivity.kt
@@ -2,28 +2,38 @@ package www.smktelkommlg.sound
 
 import android.content.Intent
 import android.media.MediaPlayer
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
+import www.smktelkommlg.sound.databinding.ActivityMainBinding
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var mediaPlayer: MediaPlayer
+
+    private lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val kobokanaer = findViewById<ImageButton>(R.id.kobokanaer)
-        mediaPlayer = MediaPlayer.create(this, R.raw.kobokanaer)
-        //Toast.makeText(this,"media playing", Toast.LENGTH_SHORT).show()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        //kobokanaer.setOnClickListener{
-        //mediaPlayer.start()
-        //}
-        kobokanaer.setOnClickListener{
-        mediaPlayer.start()
-        startActivity(Intent(this@MainActivity, kobokanaer::class.java))
-        }
+        val kobokanaer = binding.kobokanaer
+        mediaPlayer = MediaPlayer.create(this,R.raw.kobokanaer)
+
+        kobokanaer.setOnClickListener(this)
+
     }
 
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.kobokanaer -> {
+                val moveIntent = Intent(this@MainActivity, kobokanaer::class.java)
+                mediaPlayer.start()
+                startActivity(moveIntent)
+            }
+        }
+    }
 }
